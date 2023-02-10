27d4d03ec53e67e1a8091738376c31917fee945b
nawhi <25394965+nawhi@users.noreply.github.com>
2023-02-10 17:18:37 +0000

    Add initial header

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
@@ -6,7 +6,9 @@ import sitemap from '@astrojs/sitemap';
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), svelte()]
+  site: 'https://example.com', integrations: [mdx(), sitemap(), svelte(), tailwind()]
 });
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,14 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const siteMetadata = {
+    siteUrl: 'https://nickwhite.cc',
+    name: 'Nick White',
+    title: 'Nick White',
+    description: 'Software Engineer from London, UK',
+    github: 'https://github.com/nawhi',
+    linkedin: 'https://www.linkedin.com/in/nick9white/'
+};
+
+export const SITE_TITLE = siteMetadata.title;
+export const SITE_DESCRIPTION = siteMetadata.description;
