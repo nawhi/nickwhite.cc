4df9dc518ffed98bb79eeadfa57c3e2411720e04
nawhi <25394965+nawhi@users.noreply.github.com>
2023-02-11 15:38:41 +0000

    Fix order of posts; add date to PostList

--- src/utils/getPostsInOrder.ts
+++ src/utils/getPostsInOrder.ts
@@ -2,4 +2,4 @@ import {getCollection} from "astro:content";
 
 export const getPostsInOrder = () =>
     getCollection('blog')
-        .then((posts) => posts.sort((a, b) => a.data.pubDate.valueOf() - b.data.pubDate.valueOf()));
+        .then((posts) => posts.sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf()));
