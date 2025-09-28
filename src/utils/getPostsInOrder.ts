f4b061da1cb32253a51bdf3d7eb82417c2cad33e
nawhi <25394965+nawhi@users.noreply.github.com>
2025-09-28 12:32:20 +0100

    Float recently modified posts to the top

--- src/utils/getPostsInOrder.ts
+++ src/utils/getPostsInOrder.ts
@@ -1,6 +1,13 @@
 import { getCollection } from 'astro:content';
 
+type DePromisify<T> = T extends Promise<infer U> ? U : never;
+
+type Post = DePromisify<ReturnType<typeof getCollection>>[0];
+
+export const getPostDate = (post: Post) =>
+  post.data.updatedDate ?? post.data.pubDate;
+
 export const getPostsInOrder = () =>
   getCollection('blog').then((posts) =>
-    posts.sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf()),
+    posts.sort((a, b) => getPostDate(b).valueOf() - getPostDate(a).valueOf()),
   );
