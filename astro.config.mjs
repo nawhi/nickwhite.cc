1312c2b3c1478ea97cd778fa8419205193585c67
nawhi <25394965+nawhi@users.noreply.github.com>
2026-02-07 09:07:07 +0000

    Remove Svelte from the repo

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,13 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-import svelte from '@astrojs/svelte';
-
-// https://astro.build/config
 import tailwind from '@astrojs/tailwind';
 import remarkExternalLinks from 'remark-external-links';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://nickwhite.cc',
-  integrations: [mdx(), sitemap(), svelte(), tailwind()],
+  integrations: [mdx(), sitemap(), tailwind()],
   markdown: {
     syntaxHighlight: 'prism',
     remarkPlugins: [
