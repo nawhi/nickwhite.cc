a15fc2e7c7cd63c2bcc374ce6c081aa66caa5686
nawhi <25394965+nawhi@users.noreply.github.com>
2026-07-10 10:35:12 +0100

    Tweak ordering

--- src/components/about.ts
+++ src/components/about.ts
@@ -72,8 +72,8 @@ export const about = {
         'MongoDB',
       ],
       bio: [
-        'Worked on <a href="https://www.triptease.com/en/personalization">Triptease Messages</a>, including message components, a rich WYSIWYG content editor, NoSQL database and CDN, event tracking and analytics platform.',
         'Built a feed of hotel prices and scaled it from scratch to serving a million prices a day from over 1,000 hotels. The prices were sourced from user tracking, screen-scraping and algorithmic inference.',
+        'Maintained <a href="https://www.triptease.com/en/personalization">Triptease Messages</a>, including message components, a rich WYSIWYG content editor, NoSQL database and CDN, event tracking and analytics platform.',
         'Created an internal browser extension, The Integrator, still in daily use by most of the company 6 years on',
       ],
     },
